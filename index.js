fc2a06e0ff9f82ddbcbdb1759f3224d990af37c7
Nishant Bhalothia <nishantbhalothia444@gmail.com>
2024-01-03 22:19:50 +0530

    move dtabase function out of index.js file and creat login route

--- index.js
+++ index.js
@@ -1,44 +1,38 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
-const mongoose = require('mongoose');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const connectdb = require('./db/database');
+const routes = require('./routes/index');
+
 const app = express();
-
 const port = process.env.PORT || 4000;
-const router = require('./routes/index');
 
-// Connect to DB
-async function connectToDB() {
-    try {
-        await mongoose.connect(`${process.env.DB_CONNECT}/${process.env.DATABASE_NAME}`, { useNewUrlParser: true, useUnifiedTopology: true });
-        console.log('Connected to DB');
-    } catch (error) {
-        console.error('DB Connection error:', error);
-    }
-}
+// Serve static files
+app.use(express.static('public'));
 
-connectToDB();
-
-
-
-// middlewares
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
+// Enable CORS
 app.use(cors());
 
-// Import Routes
-app.use('/', router);
+// Parse incoming JSON and url-encoded requests
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+// Parse cookies
+app.use(cookieParser());
 
-// error handling
+// Mount routes
+app.use('/', routes);
+
+// Error handling middleware
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something went wrong!');
 });
 
+// Connect to DB
+connectdb.db(); // Call the db function to establish the connection to DB
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-}
-);
+});
